b92b7adf8990f81cb28397b3c6a5b8b2810289f2
BahromjonPolat <itjunior.uz@gmail.com>
2021-11-19 19:38:20 +0500

    Modellar yozildi

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        fontFamily: "GoogleSans"
       ),
       home: const SplashScreen(),
     );
--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -1,4 +1,11 @@
+import 'package:courses/components/size_config.dart';
+import 'package:courses/constants/colors.dart';
+import 'package:courses/constants/icons.dart';
+import 'package:courses/widgets/text_widgets.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -10,12 +17,56 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: _buildBody(),
+    );
   }
 
   CustomScrollView _buildBody() => CustomScrollView(
-    slivers: [
+        slivers: [
+          _buildSliverAppBar(),
+          _showSearchField(),
+        ],
+      );
 
-    ],
-  );
+  SliverAppBar _buildSliverAppBar() => SliverAppBar(
+        systemOverlayStyle: SystemUiOverlayStyle.dark,
+        backgroundColor: ConstColors.transparent,
+        toolbarHeight: getProportionateScreenHeight(103.0),
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SetTextWidget(
+              "Qanday darslar sizni \nqiziqtiradi?",
+              weight: FontWeight.w700,
+              size: 25.0,
+            ),
+            SetTextWidget("28 xil yo`nalishda darsliklar mavjud", size: 12.0)
+          ],
+        ),
+      );
+
+  _showSearchField() => SliverToBoxAdapter(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: TextField(
+            textInputAction: TextInputAction.search,
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: ConstColors.lightGrey,
+              hintText: "Izlash",
+              border: _setInputBorder(),
+              contentPadding: const EdgeInsets.symmetric(horizontal: 12.0),
+              suffixIcon: const Icon(CupertinoIcons.search),
+            ),
+          ),
+        ),
+      );
+
+  OutlineInputBorder _setInputBorder() => OutlineInputBorder(
+        borderSide: BorderSide.none,
+        borderRadius: BorderRadius.circular(10.0),
+      );
+
+
 }
